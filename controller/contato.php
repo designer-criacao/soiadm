278257248897934c623fea39af6577b8ea103bc3
designer-criacao <soidesigncriacao@gmail.com>
2019-10-12 15:29:25 -0300

    Finalizando o painel de controle do site

--- controller/contato.php
+++ controller/contato.php
@@ -18,8 +18,45 @@ if(isset($_POST['name']) && isset($_POST['email'])){
     // $gravar->PedidoGravar($nome, $email, $razao, $telefone, $regiao, $servico, $data_pedido);
 
     if($gravar->PedidoGravar()){
-        echo '<div class="more scrolly">Mensagem enviada com sucesso!</div>';
-    	Rotas::Redirecionar(2, Rotas::get_SiteHome());
+        // echo '<div class="more scrolly">Mensagem enviada com sucesso!</div>';
+        //Rotas::Redirecionar(2, Rotas::get_SiteHome());
+        
+        /*Condição para enviar pedido para o E-mail */
+        if(isset($_POST['enviar'])):
+            if(($_POST['name']) !='' && ($_POST['email']) !='' && ($_POST['mensagem']) !='' && ($_POST['razao']) !='' ):
+            $to = 'info@soidesign.com.br';
+            $subject = "Orcamento pelo Site";
+            
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $razao = $_POST['razao'];
+            $telefone = $_POST['phone'];
+            $regiao = $_POST['regiaoSampa'];
+            $servico = $_POST['servico'];
+            $decricao = $_POST['mensagem'];
+
+            $menssage = "Orçamento pelo site: 
+            Nome: $name 
+            Email: $email
+            Telefone: $telefone
+            Razão Social: $razao
+            Região: $regiao  
+            Mensagem: $decricao 
+            Tipo de Serviço: $servico
+            Data de Serviço: $data_pedido";
+            
+          mail($to, $subject, $menssage);
+                //echo "<p style='color:black' font-weigth='bold'>Mensagem enviada com sucesso!</p>";
+                echo "alert('Mensagem enviada com sucesso!')";
+                else:
+                    //echo "<p style='color:red' font-weigth='bold'>Preencha todos os campos. Por favor!</p>";
+                    echo "alert('Preencha todos os campos. Por favor!')";
+            endif;
+          endif;
+        /*Final da condição de Envio de E-mail */
+        echo '<div class="more scrolly">Mensagem enviada com sucesso!</div>';  
+        Rotas::Redirecionar(2, Rotas::get_SiteHome());
+
     }else{
         echo '<div class="more scrolly">Mensagem não enviada!';
     	Sistema::VoltarPagina();
--- model/Config.class.php
+++ model/Config.class.php
@@ -2,10 +2,10 @@
 	
 	Class Config{
 	//INFORMAÇÕES BÁSICAS DO SITE
-		const SITE_URL = "http://localhost";
+		const SITE_URL = "https://soidesign.com.br";
 		const SITE_PASTA = "soiAdmin";
 		const SITE_NOME = "SOI DESIGN";
-		const SITE_EMAIL_ADM = "soidesigncriacao@gmail.com";
+		const SITE_EMAIL_ADM = "info@soidesign.com.br";
 		//const BD_LIMITE_POR_PAG = 6;
 		//const SITE_CEP = '02810000';
 
@@ -18,21 +18,21 @@
 
 	//INFORMAÇÕES PARA O PHP MAILLER
 		const EMAIL_HOST = "smtp-mail.outlook.com";
-		const EMAIL_USER = "ivoosantos@hotmail.com";
+		const EMAIL_USER = "info@soidesign.com.br";
 		const EMAIL_NOME = "Contato Loja Soi Design";
 		const EMAIL_SENHA = "gostosao1984";
 		//const EMAIL_SENHA = "soidesign";
 		const EMAIL_PORTA = 587;
 		const EMAIL_SMTPAUTH = true;
 		const EMAIL_SMPTSECURE = "tls";
-		const EMAIL_COPIA = "ivoosantos@hotmail.com";
+		const EMAIL_COPIA = "info@soidesign.com.br";
 
 
 	//CONSTANTES PARA O PAGSEGURO
-		const PS_EMAIL  = "soidesigncriacao@gmail.com"; // email pagseguro
-	    const PS_TOKEN  = "C531D8DD789A44D4AA2EA0C0229E5445"; // token produção
-    	const PS_TOKEN_SBX = "C531D8DD789A44D4AA2EA0C0229E5445";  // token do sandbox
-    	const PS_AMBIENTE = "production";//"sandbox"; // production   /  sandbox
+		// const PS_EMAIL  = "soidesigncriacao@gmail.com"; // email pagseguro
+	    // const PS_TOKEN  = "C531D8DD789A44D4AA2EA0C0229E5445"; // token produção
+    	// const PS_TOKEN_SBX = "C531D8DD789A44D4AA2EA0C0229E5445";  // token do sandbox
+    	// const PS_AMBIENTE = "production";//"sandbox"; // production   /  sandbox
 
 
 	}
